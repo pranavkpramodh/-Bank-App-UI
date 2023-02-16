2a58c5e7f14aefd788b9eb92dc69380511dac1b2
pranavkpramodh <pranavkpramodh@gmail.com>
2023-02-17 03:23:40 +0530

    BankAppUI

---
+++ src/app/transaction/transaction.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../services/data.service';
+
+@Component({
+  selector: 'app-transaction',
+  templateUrl: './transaction.component.html',
+  styleUrls: ['./transaction.component.css']
+})
+export class TransactionComponent implements OnInit {
+
+  acno:any //hold the current acno
+  transaction:any //hold the transaction
+  length:any=[];
+
+  constructor(private ds:DataService) {//dependency injection
+    this.acno = JSON.parse(localStorage.getItem('currentAcno')||'');
+    this.ds.getTransaction(this.acno)
+    .subscribe((result:any)=>{
+      this.transaction = result.transaction;
+          
+    },
+    result => {
+      alert(result.error.message) 
+    })
+    
+   }
+
+  ngOnInit(): void {
+  }
+
+}
